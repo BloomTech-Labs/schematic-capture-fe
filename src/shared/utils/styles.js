c1cfcf81a5c69a5385da0e006ea834296ce0816c
Brad <brad@viziworx.com>
2020-01-27 15:52:17 -0500

    added base variables for sttyling

---
+++ src/shared/utils/styles.js
@@ -0,0 +1,32 @@
+export const color = {
+    // Color system
+    primary: "#0d6efd", // Blue
+    secondary: "#6c757d", // Light Grey
+    success: "#28a745", // Green
+    warning: "#ffc107", // Orange
+    danger: "#dc3545", // Danger
+
+    // Background
+    //
+    // Setting for the `<body>` element.
+    backgroundBody: "#ffffff",
+
+    // Text
+    //
+    // Style text elements.
+    textDark: "#343a40", // Dark Text
+    textLight: "#f8f9fa", // Light Text
+    textLink: "#0d6efd",
+
+    // Components
+    //
+    // Define common styles and more.
+    borderColor: "#dee2e6",
+    borderInputFocus: "#0d6efd"
+};
+
+export const font = {
+    regular:
+        'font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    size: size => `font-size: ${size}px`
+};
