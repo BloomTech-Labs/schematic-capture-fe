0080fcc5b2a3d6a130ecb9020a5863efed53c460
Brad <brad@viziworx.com>
2020-01-29 13:27:01 -0500

    finished sentry integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import * as Sentry from "@sentry/browser";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
