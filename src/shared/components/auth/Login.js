3f7cfa232e8d83b344472b477bc5eedc8d45cc74
Brad <brad@viziworx.com>
2020-01-31 11:19:01 -0500

    updated styled components, variables, added google icon and background image
    Co-authored-by: Raajn Patel <45881738+raajnpatelj@users.noreply.github.com>
    
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

--- src/shared/components/auth/Login.js
+++ src/shared/components/auth/Login.js
@@ -1,98 +1,105 @@
 import React from "react";
+import { useHistory, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
-import { useHistory, Link } from "react-router-dom";
 
 // utils
 import {
-    Container,
-    FormContainer,
-    FormGroup,
-    FieldLabel,
-    StyledField,
-    FieldError,
-    TheButtons,
-    PrimaryButton,
-    SecondaryButton,
-    LineOr
+  Container,
+  FormContainer,
+  FormGroup,
+  FieldLabel,
+  StyledField,
+  FieldError,
+  Button,
+  LineOr
 } from "./Style";
+
+import GoogleIcon from "../../assets/google-icon";
 
 // actions
 import { actions } from "../../actions/authActions";
 
 type props = {
-    background: "https://images.unsplash.com/photo-1455165814004-1126a7199f9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  background: "https://images.unsplash.com/photo-1455165814004-1126a7199f9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
 };
 
 const Login = props => {
-    const { register, handleSubmit, errors } = useForm();
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const { userLogin } = actions;
+  const { register, handleSubmit, errors } = useForm();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { userLogin, googleLogin } = actions;
 
-    const onSubmit = data => {
-        console.log(data);
-        dispatch(userLogin(data, () => history.push("/dashboard")));
-    };
+  const onSubmit = data => {
+    console.log(data);
+    dispatch(userLogin(data, history));
+  };
 
-    return (
-        <Container background="https://images.unsplash.com/photo-1455165814004-1126a7199f9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
-            <FormContainer>
-                <form className="white" onSubmit={handleSubmit(onSubmit)}>
-                    <h1 className="signin">Sign in</h1>
-                    <p>
-                        New user? <Link to="/register">Create an account</Link>
-                    </p>
-                    <FormGroup>
-                        <FieldLabel htmlFor="email">Email address</FieldLabel>
-                        {/* use aria-describedby to associate with error messages */}
-                        {/* the id field is used to associate with aria-describedby */}
-                        <StyledField
-                            type="email"
-                            name="email"
-                            id="email"
-                            aria-invalid={errors.email ? "true" : "false"}
-                            aria-describedby="error-email-required error-email-pattern"
-                            ref={register({
-                                required: true,
-                                pattern: /^\S+@\S+$/i
-                            })}
-                        />
-                        {errors.email && errors.email.type === "required" && (
-                            <FieldError id="error-email-required">
-                                Please enter an email address.
-                            </FieldError>
-                        )}
-                    </FormGroup>
-                    <FormGroup>
-                        <FieldLabel htmlFor="password">Password</FieldLabel>
-                        <StyledField
-                            type="password"
-                            name="password"
-                            id="password"
-                            aria-invalid={errors.password ? "true" : "false"}
-                            aria-describedby="error-password-required"
-                            placeholder="************"
-                            ref={register({ required: true })}
-                        />
-                        {errors.password &&
-                            errors.password.type === "required" && (
-                                <FieldError id="error-password-required">
-                                    That's an incorrect password. Try again.
-                                </FieldError>
-                            )}
-                    </FormGroup>
-                    <TheButtons>
-                        <PrimaryButton type="submit">Login</PrimaryButton>
-                    </TheButtons>
-                </form>
-                <LineOr>
-                    <p>or</p>
-                </LineOr>
-                <SecondaryButton>Continue with Google</SecondaryButton>
-            </FormContainer>
-        </Container>
-    );
+  const onGoogleLogin = event => {
+    event.preventDefault();
+
+    dispatch(googleLogin(history));
+  };
+
+  return (
+    <Container background="https://images.unsplash.com/photo-1455165814004-1126a7199f9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+      <FormContainer>
+        <form className="white" onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="signin">Sign in</h1>
+          <p>
+            New user? <Link to="/register">Create an account</Link>
+          </p>
+          <FormGroup>
+            <FieldLabel htmlFor="email">Email address</FieldLabel>
+            {/* use aria-describedby to associate with error messages */}
+            {/* the id field is used to associate with aria-describedby */}
+            <StyledField
+              type="email"
+              name="email"
+              id="email"
+              aria-invalid={errors.email ? "true" : "false"}
+              aria-describedby="error-email-required error-email-pattern"
+              ref={register({
+                required: true,
+                pattern: /^\S+@\S+$/i
+              })}
+            />
+            {errors.email && errors.email.type === "required" && (
+              <FieldError id="error-email-required">
+                Please enter an email address.
+              </FieldError>
+            )}
+          </FormGroup>
+          <FormGroup>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <StyledField
+              type="password"
+              name="password"
+              id="password"
+              aria-invalid={errors.password ? "true" : "false"}
+              aria-describedby="error-password-required"
+              ref={register({ required: true })}
+            />
+            {errors.password && errors.password.type === "required" && (
+              <FieldError id="error-password-required">
+                That's an incorrect password. Try again.
+              </FieldError>
+            )}
+          </FormGroup>
+          <Button variant="primary" type="submit">
+            Continue
+          </Button>
+        </form>
+        <LineOr>
+          <p>Or</p>
+        </LineOr>
+        <Button onClick={onGoogleLogin} variant="secondary" btnBlock>
+          <GoogleIcon />
+          Continue with Google
+        </Button>
+      </FormContainer>
+    </Container>
+  );
 };
 
 export default Login;
--- src/shared/components/auth/Style.js
+++ src/shared/components/auth/Style.js
@@ -1,129 +1,164 @@
 import styled from "styled-components";
+import Color from "color";
 
 import { color, font } from "../../../shared/utils/styles";
 
 export const Container = styled.div`
-    background: ${props => `url(${props.background})`} no-repeat center center
-        fixed;
-    background-color: #000000;
-    background-size: cover;
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    @media (max-width: 500px) {
-        background: none;
-    }
+  background: url("../../assets/8609f4b9daabe355452ccd4ea682f37e.jpg") no-repeat
+    center center fixed;
+  background-color: #000000;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 500px) {
+    background: none;
+  }
 `;
 
 export const FormContainer = styled.div`
-    background: ${color.backgroundLight};
-    border: 1px solid ${color.borderColor};
-    border-radius: 0.5rem;
-    padding: 3rem;
-    width: 400px;
-    max-width: 500px;
-    max-height: 550px;
-    position: absolute;
-    @media (min-width: 1200px) {
-        right: 370px;
+  background: ${color.backgroundLight};
+  border: 1px solid ${color.borderColor};
+  border-radius: 0.5rem;
+  padding: 4rem;
+  width: 400px;
+  max-width: 500px;
+  position: absolute;
+  h1 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  p {
+    margin-bottom: 3rem;
+  }
+  a {
+    color: ${color.primary};
+  }
+  @media (min-width: 1200px) {
+    right: 100px;
+  }
+  @media (max-width: 500px) {
+    width: 80%;
+    height: 100%;
+    border: none;
+    h1 {
+      margin-top: 3rem;
     }
-    @media (max-width: 500px) {
-        width: 80%;
-        height: 100%;
-        border: none;
-    }
+  }
 `;
 
 export const FormGroup = styled.div`
-    margin-bottom: 1rem;
+  margin-bottom: 1rem;
 `;
 
 export const FieldLabel = styled.label`
-    display: block;
-    color: ${color.textDark};
-    font-family: ${font.bold};
+  display: block;
+  color: ${Color(color.textDark)
+    .lighten(1)
+    .string()};
+  font-family: ${font.regular};
+  ${font.size(1.25)}
 `;
 
 export const StyledField = styled.input`
-    background: ${color.inputBackground};
-    border: 0.25px solid #ccc;
-    box-sizing: border-box;
-    border-radius: 0.5rem;
-    color: ${color.inputColor};
-    display: block;
-    width: 100%;
-    padding: 10px 10px;
-    margin-top: 8px;
-    &:focus {
-        background-color: #f7f7fa;
-    }
+  background: ${color.inputBackground};
+  border: 0.25px solid #ccc;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  color: ${color.inputColor};
+  display: block;
+  width: 100%;
+  padding: 10px 10px;
+  margin-top: 8px;
+  &:hover {
+    border-color:
+    ${Color(color.secondary)
+      .darken(0.1)
+      .string()};}
+  }
+  &:focus {
+    border-color: ${color.primary};
+  }
 `;
 
 export const LineOr = styled.div`
-    overflow: hidden;
-    text-align: center;
-    &:before,
-    &:after {
-        background-color: #ccc;
-        content: "";
-        display: inline-block;
-        height: 1px;
-        position: relative;
-        vertical-align: middle;
-        width: 50%;
-    }
-    &:before {
-        top: 2.15em;
-        right: 1em;
-        margin-left: -50%;
-    }
-    &:after {
-        bottom: 2.2em;
-        left: 1em;
-        margin-right: -50%;
-    }
+  overflow: hidden;
+  text-align: center;
+  &:before,
+  &:after {
+    background-color: #eaeaea;
+    content: "";
+    display: inline-block;
+    height: 1px;
+    position: relative;
+    vertical-align: middle;
+    width: 50%;
+  }
+  &:before {
+    top: 2.15em;
+    right: 1em;
+    margin-left: -50%;
+  }
+  &:after {
+    bottom: 3.25em;
+    left: 1em;
+    margin-right: -50%;
+  }
 `;
 
 export const FieldError = styled.div`
-    color: ${color.danger};
+  margin-top: 8px;
+  color: ${color.danger};
+  ${font.size(1.25)}
 `;
 
 export const Button = styled.button`
-    display: inline-block;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    background-color: transparent;
-    border: 1px solid transparent;
-    padding: 0.5rem 0.75rem;
-    font-size: 1.5rem;
-    line-height: 1.5;
-    border-radius: 0.5rem;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  cursor: pointer;
+  border: 1px solid transparent;
+  padding: 0.75rem 1rem;
+  font-size: 1.5rem;
+  line-height: 1.5;
+  transition: all 0.1s;
+  border-radius: 0.5rem;
+  ${font.bold}
 
-// A new component based on Button, but with some override styles
-export const PrimaryButton = styled(Button)`
-    color: ${color.textLight};
-    background-color: ${color.primary};
-    border-color: ${color.primary};
-    width: 40%;
+  ${props =>
+    props.btnBlock &&
+    `
+    display: block;
+    width: 100%;
+  `}
 
-    &:hover {
-        background-color: darken(${color.primary}, 25%);
+  background-color: ${({ variant }) =>
+    !variant || variant === "primary" ? color.primary : "transparent"};
+
+  color: ${({ variant }) =>
+    !variant || variant === "primary" ? color.textLight : color.textDark};
+
+  border-color: ${({ variant }) =>
+    !variant || variant === "primary" ? color.primary : color.secondary};
+
+    .icon {
+        margin-right: 12px;
+        vertical-align: sub;
     }
-`;
 
-export const SecondaryButton = styled(Button)`
-    color: ${color.textDark};
-    border-color: ${color.secondary};
-    width: 100%;
-`;
+  &:hover {
+    background-color: ${({ variant }) =>
+      variant === "primary" &&
+      Color(color.primary)
+        .darken(0.25)
+        .string()};
+    border-color: ${({ variant }) =>
+      variant === "secondary" &&
+      Color(color.secondary)
+        .darken(0.25)
+        .string()};
 
-export const TheButtons = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    padding-top: 8px;
-    width: 100%;
 `;
