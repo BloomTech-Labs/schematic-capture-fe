aa02538408eabe598e2dccfc46ce865810752c3c
Brad <brad@viziworx.com>
2020-02-04 01:21:16 -0500

    adds the useSelector hook and page loader
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

--- src/App/index.js
+++ src/App/index.js
@@ -4,11 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import NormalizeStyles from "./NormalizeStyles";
 import BaseStyles from "./BaseStyles";
 
+import PageLoader from "../shared/components/PageLoader";
+
 import Router from "./Router";
 
+// redux hooks
+import { useSelector } from "react-redux";
+
 const App = () => {
+  // const { isLoading, error } = useSelector(state => state.app);
+  const isLoading = true;
+
   return (
     <Fragment>
+      {isLoading && <PageLoader />}
       <BrowserRouter>
         <NormalizeStyles />
         <BaseStyles />
