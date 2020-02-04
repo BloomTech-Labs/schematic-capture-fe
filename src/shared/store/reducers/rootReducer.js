57f91013f4a122b105df29e272cb677dd162d7f0
Brad <brad@viziworx.com>
2020-02-04 01:28:50 -0500

    adds the app reducer to the root reducer.
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

--- src/shared/store/reducers/rootReducer.js
+++ src/shared/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
-import authReducer from './authReducer'
-import { combineReducers } from 'redux'
+import authReducer from "./authReducer";
+import appReducer from "./appReducer";
+import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
+  app: appReducer,
   auth: authReducer
 });
 
-export default rootReducer
+export default rootReducer;
 
 // the key name will be the data property on the state object
