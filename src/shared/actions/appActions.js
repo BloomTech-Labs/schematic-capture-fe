d9cb944c9cdb7fe4b60e6048256924d63e5b089d
Brad <brad@viziworx.com>
2020-02-04 01:22:35 -0500

    adds the actions for app loading, done loading, and error.
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

---
+++ src/shared/actions/appActions.js
@@ -0,0 +1,9 @@
+const APP_LOADING = "APP_LOADING";
+const APP_DONE_LOADING = "APP_DONE_LOADING";
+const APP_ERROR = "APP_ERROR";
+
+export const actions = {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  APP_ERROR
+};
