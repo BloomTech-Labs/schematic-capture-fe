60a20bec1b1c18c36bc7bf63718cb0d590e259b0
Brad <brad@viziworx.com>
2020-02-04 14:03:53 -0500

    added additional base styles

--- src/App/BaseStyles.js
+++ src/App/BaseStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-import { color, font } from "../shared/utils/styles";
+import { color, font, mixin } from "../shared/utils/styles";
 
 export default createGlobalStyle`
 
@@ -11,5 +11,48 @@ body {
     line-height: 1.2;
     ${font.size(1.5)}
     ${font.regular}
+}
+
+button,
+input,
+optgroup,
+select,
+textarea {
+  ${font.regular}
+}
+
+*, *:after, *:before, input[type="search"] {
+  box-sizing: border-box;
+}
+
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+ul, li, ol, dd, h1, h2, h3, h4, h5, h6, p {
+  padding: 0;
+  margin: 0;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+  ${font.bold}
+}
+
+button {
+  background: none;
+  border: none;
+}
+
+p {
+  line-height: 1.4285;
+  a {
+    ${mixin.link()}
+  }
 }
 `;
