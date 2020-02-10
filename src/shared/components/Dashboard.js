18c1f1abb4f332a61bba135f185a9f4a84a647ce
Brad <brad@viziworx.com>
2020-02-10 11:32:13 -0500

    added a dropdown menu

--- src/shared/components/Dashboard.js
+++ src/shared/components/Dashboard.js
@@ -1,11 +1,33 @@
 import React, { Fragment } from "react";
 import NavBar from "../components/layout/Navbar";
 
+import DropdownButton from "./DropdownButton";
+
+const items = [
+  {
+    to: "/invite/admin",
+    text: "Administration"
+  },
+  {
+    to: "/invite/employee",
+    text: "Employee"
+  },
+  {
+    to: "/invite/technician",
+    text: "Technician"
+  }
+];
+
 const Dashboard = () => {
   return (
     <Fragment>
       <NavBar />
       <h1>You made it to the dashboard!</h1>;
+      <DropdownButton
+        items={items}
+        text="Create New"
+        // buttonProps={{ variant: "secondary" }}
+      />
     </Fragment>
   );
 };
