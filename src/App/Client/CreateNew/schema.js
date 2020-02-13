b3172f108268b10ecb86c6aa556e9c78c57250b1
Brad <brad@viziworx.com>
2020-02-13 17:06:16 -0500

    adds schema for client
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

--- src/App/Client/CreateNew/schema.js
+++ src/App/Client/CreateNew/schema.js
@@ -3,8 +3,16 @@ export const schema = [
     fieldType: "input",
     name: "name",
     ref: ref => ref({ required: true }),
-    error: "This field is required.",
-    placeholder: "Company Name"
+    error: "First & Last Name or Company Name is required.",
+    type: "text",
+    placeholder: "Name"
+  },
+  {
+    fieldType: "input",
+    name: "email",
+    ref: ref => ref,
+    type: "email",
+    placeholder: "Email Address"
   },
   {
     fieldType: "input",
@@ -12,5 +20,26 @@ export const schema = [
     ref: ref => ref,
     type: "tel",
     placeholder: "Phone"
+  },
+  {
+    fieldType: "input",
+    name: "street",
+    ref: ref => ref,
+    type: "text",
+    placeholder: "Street"
+  },
+  {
+    fieldType: "input",
+    name: "city",
+    ref: ref => ref,
+    type: "text",
+    placeholder: "City"
+  },
+  {
+    fieldType: "input",
+    name: "zip",
+    ref: ref => ref,
+    type: "text",
+    placeholder: "ZIP Code"
   }
 ];
