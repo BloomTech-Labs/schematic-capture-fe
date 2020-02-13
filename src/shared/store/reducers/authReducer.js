9ba9fcec940c7eb0caf8bc88023762fd3d2e4655
Brad <brad@viziworx.com>
2020-02-13 00:08:52 -0500

    adds switch case to store google info

--- src/shared/store/reducers/authReducer.js
+++ src/shared/store/reducers/authReducer.js
@@ -1,16 +1,21 @@
 import { actions } from "../../actions/authActions";
 
-const { CREATE_ACCOUNT_SUCCESS, LOGIN_SUCCESS } = actions;
+const { CREATE_ACCOUNT_SUCCESS, LOGIN_SUCCESS, STORE_GOOGLE_INFO } = actions;
 
 const initState = {
   user: JSON.parse(localStorage.getItem("user")) || {},
-  idToken: "",
+  googleInfo: {},
   isLoading: false,
   error: null
 };
 
 const authReducer = (state = initState, action) => {
   switch (action.type) {
+    case STORE_GOOGLE_INFO:
+      return {
+        ...state,
+        googleInfo: action.payload
+      };
     case CREATE_ACCOUNT_SUCCESS:
     case LOGIN_SUCCESS:
       return {
