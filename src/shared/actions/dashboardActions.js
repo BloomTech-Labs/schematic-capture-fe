8dfc271fb24418c96a9f3bb8eba8459baf909c5f
Brad <brad@viziworx.com>
2020-02-14 01:02:56 -0500

    adds actions to fetch projects and add new project
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

--- src/shared/actions/dashboardActions.js
+++ src/shared/actions/dashboardActions.js
@@ -29,7 +29,35 @@ const addNewClient = (data, history) => dispatch => {
     });
 };
 
+const fetchProjects = (id, setProjects) => dispatch => {
+  dispatch({ type: APP_LOADING });
+  axiosWithAuth()
+    .get(`/clients/${id}/projects`)
+    .then(res => {
+      setProjects(res.data);
+      dispatch({ type: APP_DONE_LOADING });
+    })
+    .catch(error => dispatch({ type: APP_ERROR, payload: error.message }));
+};
+
+const addNewProject = (data, history, id) => dispatch => {
+  dispatch({ type: APP_LOADING });
+  axiosWithAuth()
+    .post(`/clients/${id}/projects`, data)
+    .then(project => {
+      dispatch({ type: APP_DONE_LOADING });
+      console.log(project);
+      history.push(`/clients/${id}`);
+    })
+    .catch(error => {
+      dispatch({ type: APP_ERROR });
+      console.error(error);
+    });
+};
+
 export const dispatchers = {
   addNewClient,
-  fetchClients
+  fetchClients,
+  addNewProject,
+  fetchProjects
 };
