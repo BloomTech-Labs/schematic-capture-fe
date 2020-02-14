e30ba6e0769f90a225d0f15c2f00942069f48061
Brad <brad@viziworx.com>
2020-02-14 01:15:54 -0500

    cleans up unused code and adds client component

--- src/App/Dashboard/index.js
+++ src/App/Dashboard/index.js
@@ -1,51 +1,16 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { Fragment } from "react";
 import NavBar from "./Navbar";
 
-import DropdownButton from "../../shared/components/DropdownButton";
+import Clients from "../Client";
 import Router from "./Router";
 
-import { dispatchers } from "../../shared/actions/dashboardActions";
-
-const { fetchClients } = dispatchers;
-
-const items = [
-  {
-    to: "/invite/admin",
-    text: "Administration"
-  },
-  {
-    to: "/invite/employee",
-    text: "Employee"
-  },
-  {
-    to: "/invite/technician",
-    text: "Technician"
-  }
-];
-
 const Dashboard = () => {
-  const [clients, setClients] = useState([]);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchClients(setClients));
-  }, []);
-
   return (
     <Fragment>
       <NavBar />
-      <h1>You made it to the dashboard!</h1>;
-      <DropdownButton
-        items={items}
-        text="Create New"
-        aria-haspopup="true"
-        aria-expanded="false"
-      />
-      {clients.map(client => (
-        <pre>{JSON.stringify(client, null, 2)}</pre>
-      ))}
+      <h1>Dashboard</h1>
       <Router />
+      <Clients />
     </Fragment>
   );
 };
