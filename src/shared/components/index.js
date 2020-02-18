426829a4fbdc243c955eae0836126a9ff7805923
Brad <brad@viziworx.com>
2020-02-17 19:59:33 -0500

    adds export defaults for shared components

---
+++ src/shared/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Button";
+export { default as Form } from "./Form";
+export { default as Input } from "./Input";
+export { default as PageError } from "./PageError";
+export { default as PageLoader } from "./PageLoader";
+export { default as Spinner } from "./Spinner";
+export { default as TextArea } from "./TextArea";
