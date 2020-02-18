cdc2c9dc79d71bcdd92fe03da8a04434466e436f
Brad <brad@viziworx.com>
2020-02-18 18:24:23 -0500

    removes redux actions and useEffect
    Co-authored-by: Nattajohn Rojanasupya <45881738+nattroj@users.noreply.github.com>

--- src/App/Dashboard/index.js
+++ src/App/Dashboard/index.js
@@ -1,20 +1,8 @@
-import React, { Fragment, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Fragment } from "react";
 import NavBar from "../../shared/components/Navbar";
 import Board from "./Board";
 
-import { dispatchers } from "../../shared/actions/dashboardActions";
-
-const { fetchClients } = dispatchers;
-
 const Dashboard = () => {
-  const dispatch = useDispatch();
-  const clients = useSelector(state => state.dashboard.clients);
-
-  useEffect(() => {
-    dispatch(fetchClients());
-  }, [dispatch]);
-
   return (
     <Fragment>
       <NavBar />
