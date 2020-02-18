87f437c5b0904b65789294d023002c4ead641018
Brad <brad@viziworx.com>
2020-02-17 19:42:06 -0500

    updated background color

--- src/shared/components/PageLoader/Styles.js
+++ src/shared/components/PageLoader/Styles.js
@@ -4,7 +4,7 @@ import { color, mixin } from "../../utils/styles";
 
 export default styled.div`
   ${mixin.cover}
-  background-color: ${color.backgroundLight};
+  background-color: ${color.backgroundDark};
   z-index: 1;
   padding: 225px 0;
 `;
