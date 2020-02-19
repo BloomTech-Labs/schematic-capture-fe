2eb6f9ff4cf8a5cbe2adcf24f104dfe96f1fe874
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2020-02-19 15:21:18 -0800

    adds sanitizing logic for components array before getting sent to API

---
+++ src/shared/utils/componentMap.js
@@ -0,0 +1,35 @@
+export const apiToDisplay = {
+  componentId: "Component",
+  rlCategory: "RL Category",
+  rlNumber: "RL Number",
+  descriptions: "Description",
+  manufacturer: "Manufacturer",
+  partNumber: "Part Number",
+  stockCode: "Stock Code",
+  electricalAddress: "Electrical Address",
+  componentApplication: "Component Application",
+  referenceTag: "Reference Tag",
+  settings: "Settings",
+  image: "Image",
+  resources: "Resources",
+  cutsheet: "Cutsheet",
+  maintenanceVideo: "Maintenance Video"
+};
+
+export const csvToApi = {
+  Component: "componentId",
+  "RL Category": "rlCategory",
+  "RL Number": "rlNumber",
+  Description: "descriptions",
+  Manufacturer: "manufacturer",
+  "Part Number": "partNumber",
+  "Stock Code": "stockCode",
+  "Electrical Address": "electricalAddress",
+  "Component Application": "componentApplication",
+  "Reference Tag": "referenceTag",
+  Settings: "settings",
+  Image: "image",
+  Resources: "resources",
+  Cutsheet: "cutsheet",
+  "Maintenance Video": "maintenanceVideo"
+};
