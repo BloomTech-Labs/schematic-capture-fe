e6868f371baf6dd0b5757eced0fd352ba1e9240d
Brad <brad@viziworx.com>
2020-02-20 12:08:10 -0500

    adds styling to new project button component, including flexbox
    Co-authored by: Nattajohn Rojanasupya <n.rojanasupya@gmail.com>

--- src/App/Client/Board/Header/Styles.js
+++ src/App/Client/Board/Header/Styles.js
@@ -1,12 +1,22 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import { font } from "../../../../shared/utils/styles";
+import { buttonStyle } from "../../../../shared/components/Button/Styles";
 
 export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin-bottom: 1rem;
+  padding-right: 2rem;
 `;
 
 export const PageName = styled.h1`
   ${font.size(3)}
   ${font.bold}
+  margin-right: 1rem;
+`;
+
+export const StyledLink = styled(Link)`
+  ${buttonStyle}
 `;
