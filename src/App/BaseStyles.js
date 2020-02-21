db7339f8997defc17700bd9f81073dfa25e81e12
Brad <brad@viziworx.com>
2020-02-21 16:36:43 -0500

    adds base styling for tables

--- src/App/BaseStyles.js
+++ src/App/BaseStyles.js
@@ -56,4 +56,87 @@ p {
     ${mixin.link()}
   }
 }
+
+table {
+  border: 0;
+  border-bottom: 1px solid ${color.gray200};
+  border-collapse: collapse;
+  border-spacing: 0;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
+}
+
+table caption {
+  font-size: 1.5em;
+  margin: .5em 0 .75em;
+  text-align: left;
+}
+
+table tr {
+  border: 1px solid ${color.gray200};
+  padding: .35em;
+}
+
+table th,
+table td {
+  padding: .625em;
+}
+
+table th {
+  font-size: .85em;
+  text-align: left;
+  opacity: 0.25;
+}
+
+.secondary { opacity: 0.5; font-size: 14px; }
+
+@media screen and (max-width: 600px) {
+  table {
+    border: 0;
+  }
+
+  table thead {
+    border: none;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+
+
+  table tr {
+    border-bottom: 3px solid ${color.gray200};
+    display: block;
+    padding-bottom: 1em;
+    margin-bottom: .625em;
+  }
+
+  table td {
+    border-bottom: 1px solid ${color.gray200};
+    display: block;
+    font-size: .8em;
+    text-align: right;
+  }
+
+  table td::before {
+    /*
+    * aria-label has no advantage, it won't be read inside a table
+    content: attr(aria-label);
+    */
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  table td:last-child {
+    border-bottom: 0;
+  }
+}
+
 `;
