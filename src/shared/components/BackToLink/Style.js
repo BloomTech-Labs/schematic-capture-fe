8ec99b9dbe9a13ef13d518bd6f6bf3581681b678
Brad <brad@viziworx.com>
2020-02-26 16:17:50 -0500

    removes hover state

--- src/shared/components/BackToLink/Style.js
+++ src/shared/components/BackToLink/Style.js
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import { color, mixin } from "../../utils/styles";
+import { color } from "../../utils/styles";
 
 export const StyledLink = styled(Link)`
   display: flex;
   align-items: center;
   color: ${color.primaryLight};
-  &:hover {
-    color: ${mixin.darken(color.primaryLight, 0.1)};
-  }
   .icon {
     margin-right: 8px;
   }
