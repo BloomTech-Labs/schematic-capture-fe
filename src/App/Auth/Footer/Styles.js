e673d08bda56fe2706cf643f57b17535380a7f4b
Brad <brad@viziworx.com>
2020-02-26 16:22:34 -0500

    adds footer component with styling

---
+++ src/App/Auth/Footer/Styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+import { color } from "../../../shared/utils/styles";
+
+export const StyledNav = styled.nav`
+  align-items: center;
+  border: 0px solid black;
+  box-sizing: border-box;
+  display: flex;
+  flex-basis: auto;
+  flex-shrink: 0;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0px;
+  min-height: 0px;
+  min-width: 0px;
+  padding: 2rem;
+  position: relative;
+  color: ${color.gray200};
+`;
