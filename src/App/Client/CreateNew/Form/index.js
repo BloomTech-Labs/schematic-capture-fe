a294172ef9724f56076f9b497682deb2c481adf2
rndpatel@gmail.com <rndpatel@gmail.com>
2020-02-28 09:57:10 -0800

    new client number input removed and replaced with string regex

--- src/App/Client/CreateNew/Form/index.js
+++ src/App/Client/CreateNew/Form/index.js
@@ -29,15 +29,31 @@ const CreateNewClient = () => {
   useEffect(() => {
     inputElement = document.getElementById("phone");
     inputElement.addEventListener("keyup", formatToPhone);
+
+    inputElement = document.getElementById("zip");
+    inputElement.addEventListener("keyup", formatToZip);
   });
 
   const onAddNewClient = data => {
     dispatch(addNewClient(data, history));
   };
 
+  const formatToZip = event => {
+    const target = event.target;
+    let input = target.value.replace(/\D/g, "").substring(0, 9);
+    const zip = input.substring(0, 5);
+    const addon = input.substring(5, 9);
+
+    if (input.length > 5) {
+      target.value = `${zip} - ${addon}`;
+    } else {
+      target.value = `${zip}`;
+    }
+  };
+
   const formatToPhone = event => {
     const target = event.target;
-    const input = target.value.replace(/\D/g, "").substring(0, 10); // First ten digits of input only
+    const input = target.value.replace(/\D/g, "").substring(0, 10);
     const zip = input.substring(0, 3);
     const middle = input.substring(3, 6);
     const last = input.substring(6, 10);
@@ -103,7 +119,7 @@ const CreateNewClient = () => {
               ref={register({})}
             />
             <StyledField
-              type="number"
+              type="string"
               name="zip"
               id="zip"
               placeholder="Zip Code"
