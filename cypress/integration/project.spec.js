205be2ec9e525aaae58da400e0ad809a7de0c042
adamwinzdesign <adamwinzdesign@gmail.com>
2020-04-08 17:46:31 -0500

    added newClient spec file and added tests

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -9,6 +9,7 @@ describe('Test project page functionality', function() {
     it('Displays items on the project page correctly', function() {
       cy.url().should('include', '/project')
       cy.contains('Testing 3')
+      cy.contains('All Jobsheets')
       cy.contains('testing 3')
       cy.contains('Renamed version')
       cy.contains('New Jobsheet')
