aeb2f2bb2a450935107a326973a9b8cc09b713fc
adamwinzdesign <adamwinzdesign@gmail.com>
2020-04-09 17:16:21 -0500

    added to commands, dashboard, newClient, also added testing for items saved to local storage

--- cypress/integration/newClient.spec.js
+++ cypress/integration/newClient.spec.js
@@ -4,28 +4,39 @@ describe('Test New Client form', function() {
       cy.loginScript();
     });
 
-    it('navigates from dashboard to new client form', function() {
+    it('navigates from dashboard to new client form and displays form contents', function() {
       cy.contains('New Client').click();
       cy.url().should('include', 'client/new')
       cy.contains('Create a New Client')
       cy.get('#companyName')
         .should('have.attr', 'name', 'companyName')
+        .and('have.attr', 'type', 'string')
         .and('have.attr', 'placeholder', 'Company Name')
       cy.get('#phone')
         .should('have.attr', 'name', 'phone')
+        .and('have.attr', 'type', 'tel')
         .and('have.attr', 'placeholder', 'Phone Number')
       cy.get('#street')
         .should('have.attr', 'name', 'street')
+        .and('have.attr', 'type', 'string')
         .and('have.attr', 'placeholder', 'Street')
       cy.get('#city')
         .should('have.attr', 'name', 'city')
+        .and('have.attr', 'type', 'string')
         .and('have.attr', 'placeholder', 'City')
       cy.get('#state')
         .should('have.attr', 'name', 'state')
+        .and('have.attr', 'type', 'string')
         .and('have.attr', 'placeholder', 'State')
       cy.get('#zip')
         .should('have.attr', 'name', 'zip')
+        .and('have.attr', 'type', 'string')
         .and('have.attr', 'placeholder', 'Zip Code')
+      cy.get('Button')
+      // navigate back to dashboard
+      cy.get('.sc-AxjAm')
+        .click()
+      cy.url().should('contain', '/dashboard')
     })
   })
 })
