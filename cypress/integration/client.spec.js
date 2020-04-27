6cb4994719d44dd0091867d5c6c27db74038d072
adamwinzdesign <adamwinzdesign@gmail.com>
2020-04-27 12:40:42 -0500

    updated client spec

--- cypress/integration/client.spec.js
+++ cypress/integration/client.spec.js
@@ -6,13 +6,28 @@ describe('Test client page functionality', function() {
     });
 
     it('navigates to Test Client 1 and displays correct content', function() {
-      cy.contains('Test Client 1')
-      cy.contains('testing 3')
-      cy.contains('table shenanigans')
-      cy.contains('tyler')
-      cy.contains('where is the html tag')
-      cy.contains('ProjectTesting')
-      cy.contains('Test Project 1')
+      // client name
+      cy.get('.sc-fzqARJ')
+      // New Project button
+      cy.get('.sc-fzoNJl')
+      // Projects title bar
+      cy.get('.sc-fzqNqU')
+      // Name header
+      cy.get('thead > .sc-fznMAR > :nth-child(1)')
+      // Description header
+      cy.get('thead > .sc-fznMAR > :nth-child(1)')
+      // Technician header
+      cy.get('thead > .sc-fznMAR > :nth-child(3)')
+      // Jobsheet header
+      cy.get('thead > .sc-fznMAR > :nth-child(4)')
+      // Status header
+      cy.get('thead > .sc-fznMAR > :nth-child(5)')
+      // First entry in projects table
+      cy.get(':nth-child(1) > :nth-child(1) > .sc-fzqBkg')
+      // Second entry in projects table
+      cy.get(':nth-child(1) > :nth-child(1) > .sc-fzqBkg')
+      // Table row
+      cy.get('tbody > :nth-child(1) > :nth-child(1)')
       cy.url().should('include', '/client')
     }) 
 
