00a6d6c669d5e0fa1dcb113b50111c1b3cc06dcf
adamwinzdesign <adamwinzdesign@gmail.com>
2020-04-27 14:00:27 -0500

    updated dashboard and project specs

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -6,9 +6,15 @@ describe("User at Dashboard", function() {
 
     it('Displays dashboard info correctly', function() {
       cy.url().should('include', '/dashboard')
+      // Clients header
+      cy.get('.sc-fznMnq')
+      // First client name
       cy.get(':nth-child(1) > .sc-fzomME')
+      // Second client name
       cy.get(':nth-child(2) > .sc-fzomME')
+      // First client completed status
       cy.get(':nth-child(1) > .sc-fzoJMP')
+      // Second client completed status
       cy.get(':nth-child(2) > .sc-fzoJMP')
     })
 
--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -8,9 +8,36 @@ describe('Test project page functionality', function() {
 
     it('Displays items on the project page correctly', function() {
       cy.url().should('include', '/project')
-      cy.contains('Testing 3')
-      cy.contains('All Jobsheets')
-      cy.contains('New Jobsheet')
+      // Top container
+      cy.get('.sc-fzoiQi')
+      // Completed status container
+      cy.get('.sc-fzoiQi > :nth-child(1)')
+      // Overall completed status indicator
+      cy.get('h4')
+      // Button container
+      cy.get('.sc-fzqOul')
+      // Assign techs button
+      cy.get('[href=""]')
+      // New jobsheet button
+      cy.get('[variant="primary"]')
+      // First jobsheet name in table
+      cy.get(':nth-child(1) > :nth-child(1) > .sc-fznOgF')
+      // First jobsheet status indicator
+      cy.get(':nth-child(1) > :nth-child(4) > .sc-fznYue')
+      // Second jobsheet name in table
+      cy.get(':nth-child(2) > :nth-child(1) > .sc-fznOgF')
+      // Second jobsheet status indicator
+      cy.get(':nth-child(2) > :nth-child(4) > .sc-fznYue')
+      // All jobsheets header container
+      cy.get('.sc-fzqLLg')
+      // Name header
+      cy.get('thead > .sc-fzplgP > :nth-child(1)')
+      // Description header
+      cy.get('thead > .sc-fzplgP > :nth-child(2)')
+      // Techs header
+      cy.get('thead > .sc-fzplgP > :nth-child(3)')
+      // Status header
+      cy.get('thead > .sc-fzplgP > :nth-child(3)')
     })
     
     it('captures correct location information', function() {
