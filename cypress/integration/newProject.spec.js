069e277cee404b720197ab1a85ac8552d51bf0e4
adamwinzdesign <adamwinzdesign@gmail.com>
2020-04-28 15:04:10 -0500

    updated selectors

--- cypress/integration/newProject.spec.js
+++ cypress/integration/newProject.spec.js
@@ -4,13 +4,24 @@ describe("Testing New Project Button and Form", function() {
       cy.clientToNewProject();
     });
 
+    it('displays the ProjectHeader correctly', function() {
+      cy.get('.sc-fzqPZZ');
+      // search widget
+      cy.get('.sc-fzqAbL')
+      // user widget
+      cy.get('.sc-fzpkJw')
+    })
+
     it('displays the New Project Form correctly', function() {
       cy.url().should('include', '/project/new')
       cy.contains('Please enter the project name below:')
       cy.get('form').should('be.visible')
+      // Paragraph with instructions
       cy.get('p').should('be.visible')
-      cy.get('input').should('be.visible')
-      cy.get('button').should('be.visible')
+      // Project name input
+      cy.get('form > :nth-child(2)')
+      // Button
+      cy.get('form > :nth-child(3) > button')
     })
 
     it('assigns correct attributes to the input', function() {
