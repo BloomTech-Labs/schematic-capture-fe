0a2721f67d508fa60d336c633f94e2d442af6c42
Timothy <timgill987@gmail.com>
2020-05-26 12:53:45 -0400

    fixed testing issue maybe?

--- src/shared/utils/axiosWithAuth.js
+++ src/shared/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const idToken = localStorage.getItem("idToken");
   const BASE_URL =
     process.env.REACT_APP_BASE_URL ||
-    "http://localhost:5435/api";
+    "https://schematiccapture-master.herokuapp.com/api";
 
   return axios.create({
     baseURL: BASE_URL,
