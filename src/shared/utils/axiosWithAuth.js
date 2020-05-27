49fb470e98ee5250eb8efa60ec69373ee7a6dbc7
Timothy Gilliam <timgill987@gmail.com>
2020-05-27 13:34:30 -0400

    invite user tested on deployed BE WORKS!

--- src/shared/utils/axiosWithAuth.js
+++ src/shared/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const idToken = localStorage.getItem("idToken");
   const BASE_URL =
     process.env.REACT_APP_BASE_URL ||
-    "https://schematiccapture-master.herokuapp.com/api";
+    "https://schematic-capture.herokuapp.com/api/";
 
   return axios.create({
     baseURL: BASE_URL,
