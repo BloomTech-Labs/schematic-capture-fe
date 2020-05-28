9bc7c30f3cda90402298b77a73c053f6c68e0c22
Timothy Gilliam <timgill987@gmail.com>
2020-05-28 16:57:06 -0400

    working dropbox schematic thing maybe?

--- src/App/Jobsheet/CreateNew/DropboxSchematicChooser.js
+++ src/App/Jobsheet/CreateNew/DropboxSchematicChooser.js
@@ -1,22 +1,28 @@
-import React from "react"
-import { useDropboxChooser } from 'use-dropbox-chooser'
-import styled from 'styled-components';
-import Picture from '../JobsheetBoard/Camera.png'
+import React from "react";
+import { useDropboxChooser } from "use-dropbox-chooser";
+import styled from "styled-components";
+import Picture from "../JobsheetBoard/Camera.png";
 import { axiosWithAuth } from "../../../shared/utils/axiosWithAuth";
 function Dropbox() {
   const { open, isOpen } = useDropboxChooser({
-    appKey: 't5i27y2t3fzkiqj',
-    chooserOptions: { multiple: true, linkType: 'direct' },
-    onSelected: files => {
-      console.log(files)
-      axiosWithAuth().post("/jobsheets/create", files.link)
+    appKey: "t5i27y2t3fzkiqj",
+    chooserOptions: { multiple: true, linkType: "direct" },
+    onSelected: (files) => {
+      console.log(files);
+      axiosWithAuth()
+        .post("/create", files[0].link)
+        .then((res) => {
+          const link = res.files.link;
+          localStorage.setItem("schematic", link);
+        })
+        .catch((err) => console.log(err));
     },
-  })
+  });
   return (
     <Button onClick={open} disabled={isOpen}>
       Add Schematic
     </Button>
-  )
+  );
 }
 export default Dropbox;
 
@@ -27,4 +33,5 @@ const Button = styled.button`
   align-items: center;
   img {
     width: 100%;
-  }`
+  }
+`;
