21af1de7fa1ae8539482350ee88ffbf1ae8e76ec
Timothy Gilliam <timgill987@gmail.com>
2020-06-02 12:05:30 -0400

    fixed merge conflict in client.js

--- src/App/Dashboard/Clients.js
+++ src/App/Dashboard/Clients.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { dispatchers } from "../../shared/actions/dashboardActions"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { dispatchers } from "../../shared/actions/dashboardActions";
 
 import {
   Section,
@@ -10,75 +10,62 @@ import {
   ClientBox,
   Spacer,
   FlexEnd,
-
   FlexEnd2,
-
   NewClientBtn,
+} from "../Styles/Dashboard";
 
-} from '../Styles/Dashboard'
+import { ClientHeaderContain } from "../Styles/Client";
 
-import {
-  ClientHeaderContain,
-} from '../Styles/Client';
+const { fetchClients } = dispatchers;
 
-const { fetchClients } = dispatchers
+const Clients = (props) => {
+  const dispatch = useDispatch();
+  const clients = useSelector((state) => state.dashboard.clients);
 
-const Clients = props => {
-  const dispatch = useDispatch()
-  const clients = useSelector(state => state.dashboard.clients)
-
-  console.log(clients)
+  console.log(clients);
 
   useEffect(() => {
-    dispatch(fetchClients())
-  }, [])
+    dispatch(fetchClients());
+  }, []);
 
- 
   var clientArray;
-  if (props.search.length > 0){
-    clientArray = props.clientsSrc
+  if (props.search.length > 0) {
+    clientArray = props.clientsSrc;
   } else {
-    clientArray = clients
+    clientArray = clients;
   }
 
   return (
     <Section>
-  
-
-        <ClientHeaderContain>
-          <Clientsh2>Clients</Clientsh2>
-            <NewClientBtn
-              to={`/client/new`}
-              variant="primary"
-              >
-              New&nbsp;Client
-            </NewClientBtn>
-        </ClientHeaderContain>
+      <ClientHeaderContain>
+        <Clientsh2>Clients</Clientsh2>
+        <NewClientBtn to={`/client/new`} variant="primary">
+          New&nbsp;Client
+        </NewClientBtn>
+      </ClientHeaderContain>
       <LineBreak />
       {/* @TODO: Please make cleaner sometime */}
-      {props.search.length>0 ? 
 
-        <ClientCont>
-        {clientArray.map(client => (
-            <Spacer>
-              <ClientBox
-                data-client-name
-                key={client.id}
-                to={`/client/${client.id}`}
-              >
-                {client.companyName}
-              </ClientBox>
-              {client.completed !== 1  ? (
-                    <FlexEnd>Incomplete</FlexEnd>
-                  ) : (
-                    <FlexEnd2>Complete</FlexEnd2>
-                  )}
-            </Spacer>
-          ))}
-        </ClientCont> 
-      
+      <ClientCont>
+        {clientArray.map((client) => (
+          <Spacer>
+            <ClientBox
+              data-client-name
+              key={client.id}
+              to={`/client/${client.id}`}
+            >
+              {client.companyName}
+            </ClientBox>
+            {client.completed !== 1 ? (
+              <FlexEnd>Incomplete</FlexEnd>
+            ) : (
+              <FlexEnd2>Complete</FlexEnd2>
+            )}
+          </Spacer>
+        ))}
+      </ClientCont>
     </Section>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
