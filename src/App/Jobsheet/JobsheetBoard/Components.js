d99a55eec856492aa13989fa034167214521e6da
CeeDeeBee <cdbarnes98@gmail.com>
2020-06-16 12:17:43 -0400

    Fix download CSV headers

--- src/App/Jobsheet/JobsheetBoard/Components.js
+++ src/App/Jobsheet/JobsheetBoard/Components.js
@@ -3,6 +3,7 @@ import { useParams, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import SortDropDown from "../../../shared/components/Components/SortDropDown.js";
+import { headerKeys } from "../../../shared/utils/componentMap";
 
 import EditComponents from "./EditComponents.js";
 
@@ -147,7 +148,9 @@ const Components = (props) => {
     <section>
       {/* <Status>Complete: {currentJobsheet.tally}</Status> */}
       <Wrapper>
-        <CSVLink data={components}>Download CSV</CSVLink>
+        <CSVLink data={components} headers={headerKeys}>
+          Download CSV
+        </CSVLink>
         <ImgWrapper>
           <Sorticon>
             <Buttin className="Sort">
